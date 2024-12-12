7fa94f91631a2c61bf3b304ddea3aad42cb666c1
vvvelikaya <nik.velikay@gmail.com>
2024-12-13 00:16:26 +0200

    added init flyway script

---
+++ src/main/resources/db/migration/V1__create_todo_table.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA IF NOT EXISTS todo;
+
+CREATE TABLE IF NOT EXISTS todo
+(
+    id              BIGSERIAL PRIMARY KEY,
+    description     VARCHAR(255) NOT NULL,
+    check_mark      BOOLEAN      NOT NULL DEFAULT FALSE,
+    completion_date TIMESTAMP,
+    due_date        TIMESTAMP,
+    creation_date   TIMESTAMP
+);
