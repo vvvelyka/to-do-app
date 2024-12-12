5f2992f2e3c26b3c6eb0386fb517a98c0c37ddae
vvvelikaya <nik.velikay@gmail.com>
2024-12-13 00:15:48 +0200

    created todo entity

---
+++ src/main/java/com/project/todo/domain/ToDo.java
@@ -0,0 +1,23 @@
+package com.project.todo.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@Table(name = "todo")
+@Entity
+public class ToDo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String description;
+    private boolean checkMark = false;
+    private LocalDateTime completionDate;
+    private LocalDateTime dueDate;
+    private LocalDateTime creationDate;
+}
