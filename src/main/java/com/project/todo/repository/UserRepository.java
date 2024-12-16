39b197484c4f7fb0cdb2ae0520f94f34a436557e
vvvelikaya <nik.velikay@gmail.com>
2024-12-17 00:35:09 +0200

    created user repo

---
+++ src/main/java/com/project/todo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.project.todo.repository;
+
+import com.project.todo.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+}
