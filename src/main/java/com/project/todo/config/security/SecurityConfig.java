2655f3c4df789e0c063b39a1a3d72e997a749f73
vvvelikaya <nik.velikay@gmail.com>
2024-12-17 00:37:01 +0200

    added security config

---
+++ src/main/java/com/project/todo/config/security/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.project.todo.config.security;
+
+import com.project.todo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static java.lang.String.format;
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/api/login", "/api/signup") // Disable CSRF for these endpoints
+                )
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(STATELESS))
+                .httpBasic(withDefaults())
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
+                        .jwt(withDefaults())
+                )
+                .userDetailsService(username -> userRepository.findByUsername(username)
+                        .map(user -> User.builder()
+                                .username(user.getUsername())
+                                .password(user.getPassword())
+                                .build())
+                        .orElseThrow(
+                                () -> new UsernameNotFoundException(
+                                        format("User: %s, not found", username)
+                                )
+                        )
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/signup").permitAll()
+                                .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
