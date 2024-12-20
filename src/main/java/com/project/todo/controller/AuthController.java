6b4798ced8f9fa43501a66004e08d77d9f32207d
vvvelyka <nik.velikay@gmail.com>
2024-12-20 23:46:20 +0200

    added javadoc for auth controller

--- src/main/java/com/project/todo/controller/AuthController.java
+++ src/main/java/com/project/todo/controller/AuthController.java
@@ -14,6 +14,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller for handling authentication-related requests.
+ * Provides endpoints for user registration and login.
+ */
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
@@ -23,12 +27,28 @@ public class AuthController {
     private final UserService userService;
     private final AuthenticationService authenticationService;
 
-    @PostMapping("/signup")
+    /**
+     * Registers a new user.
+     *
+     * @param userRegistrationDto the user DTO with credentials to register.
+     * @return {@code 200} with registered user details in the body,
+     *         {@code 400} if the user already exists or provided data is invalid or,
+     *         {@code 500} if there is an internal server error.
+     */
+    @PostMapping("/sign-up")
     public UserDto signup(@RequestBody @Valid UserRegistrationDto userRegistrationDto) {
         log.info("Signup request received for username: {}", userRegistrationDto.getUsername());
         return userService.createUser(userRegistrationDto);
     }
 
+    /**
+     * Authenticates a user Basic HTTP authentication.
+     *
+     * @param authentication the authentication object containing the user's credentials.
+     * @return {@code 200} with JWT access token in the body
+     *         {@code 401} if the user credentials invalid
+     *         {@code 500} if there is an internal server error.
+     */
     @PostMapping("/login")
     public TokenResponseDto login(Authentication authentication) {
         log.info("Login request received for username: {}", authentication.getName());
